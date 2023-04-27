b903ae9e74f0c266d9a398419474ac8c689fd908
Harvey Yerik <yerik05vh@gmail.com>
2023-04-26 22:28:37 -0500

    dev: update project

--- src/components/app/Main/Sections/ScreenOne.tsx
+++ src/components/app/Main/Sections/ScreenOne.tsx
@@ -5,7 +5,7 @@ const ScreenOne = () => {
   return (
     <style.SreenOneStyles>
       <style.NoThereHabits>
-        <IconPlant size={150} />
+        <IconPlant size={150} color='#363d4f' />
         <style.HeadlineNoHabits>Cultiva un nuevo habito</style.HeadlineNoHabits>
         <style.TextBodyOne>
           Cada hábito que cultivas es una semilla que plantas para tu futuro. ¡Cultiva uno nuevo hoy
