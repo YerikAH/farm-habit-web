036b0226cd6fe99fb2fc71d2e5d355e09c0e2f15
Harvey Yerik <yerik05vh@gmail.com>
2023-05-26 22:25:14 -0500

    :bug: Fixed drag and drop operation.

--- src/components/Page/ImagesDrop/ImagesDrop.tsx
+++ src/components/Page/ImagesDrop/ImagesDrop.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { order } from '../../../helpers/helpers'
 import { IMAGES_DESKTOP, InterfaceDesktopImages } from './images_drop'
 import s from './images_drop.module.css'
+import './images_drop.css'
 
 const ImagesDrop = () => {
   const [images, setImages] = useState<InterfaceDesktopImages[]>(IMAGES_DESKTOP)
@@ -10,34 +11,28 @@ const ImagesDrop = () => {
     <DragDropContext
       // eslint-disable-next-line no-console
       onDragEnd={(result) => {
-        const { source ,  destination} = result
-        if(!destination) return
-        if(source.index === destination.index && source.droppableId === destination.droppableId) return
-        setImages((image) => 
-          order(image, source.index, destination.index)
-        )
+        const { source, destination } = result
+        if (!destination) return
+        if (source.index === destination.index && source.droppableId === destination.droppableId)
+          return
+        setImages((image) => order(image, source.index, destination.index))
       }}
-
     >
-      <Droppable droppableId="images" direction='horizontal'>
-        {( droppableProvided) => (
-          <div
-          {...droppableProvided.droppableProps}
-          ref={droppableProvided.innerRef}
-          >
+      <Droppable droppableId='images' direction='horizontal'>
+        {(droppableProvided) => (
+          <div {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
             {images.map((item, idx) => (
-              <Draggable
-                key={item.id}
-                draggableId={`${item.id}`}
-                index={idx}
-              >
+              <Draggable key={item.id} draggableId={`${item.id}`} index={idx}>
                 {(draggableProvided) => (
                   <div
                     {...draggableProvided.draggableProps}
                     {...draggableProvided.dragHandleProps}
                     ref={draggableProvided.innerRef}
                   >
-                    <div style={{backgroundImage: `url("${item.image}")`}} className={s.image_responsive}></div> 
+                    <div
+                      style={{ backgroundImage: `url("${item.image}")` }}
+                      className={`${s.image_responsive} webpage_image_responsive--${idx}`}
+                    ></div>
                   </div>
                 )}
               </Draggable>
