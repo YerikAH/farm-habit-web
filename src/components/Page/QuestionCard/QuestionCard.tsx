a235d6317dac1c58adb73517b97c5195e90952a2
YerikAH <yerik05vh@gmail.com>
2023-06-11 13:20:28 -0500

    :sparkles: Add question card component.

--- src/components/Page/QuestionCard/QuestionCard.tsx
+++ src/components/Page/QuestionCard/QuestionCard.tsx
@@ -1,18 +1,27 @@
 import { IconChevronDown } from '@tabler/icons-react'
 import { QuestionCardProps } from './props'
 import s from './question_card.module.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const QuestionCard = ({ title, text }: QuestionCardProps) => {
   const [show, setShow] = useState(false)
+  const [styles, setStyles] = useState('')
 
   const handleClick = () => setShow(!show)
 
+  const toggleArrow = () => {
+    if(show) setStyles('--active')
+    else setStyles('')
+  }
+
+  useEffect(() => {
+    toggleArrow()
+  }, [show]) 
   return (
     <div className={s.card}>
       <div className={s.card_title}>
         <h6>{title}</h6>
-        <button onClick={handleClick}>
+        <button onClick={handleClick} className={s[styles]}>
           <IconChevronDown />
         </button>
       </div>
