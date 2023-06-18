9048c747389e2dc84b4338507557044f8671dffa
YerikAH <yerik05vh@gmail.com>
2023-06-18 18:37:28 -0500

    :white_check_mark: (FarmHabitWeb): Start make testing in project, component Navigation.

---
+++ src/components/Page/Navigation/Navigation.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Navigation from './Navigation'
+
+describe('Navigation', () => {
+  test('Open test', () => {
+    const component = render(<Navigation/>)
+  })
+})
